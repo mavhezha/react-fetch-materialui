72b2144fc4046489908eb806bfcd79a1151f9cd9
Arnold Mavhezha <arnoemavhezha@gmail.com>
2023-05-17 19:07:50 +0200

    Rendering Data Dynamically Using a REST API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { DataGrid } from '@mui/x-data-grid';
+import { createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  // By enabling this option, if you have non-MUI elements (e.g. `<div />`)
+  // using MUI classes (e.g. `.MuiButton`) they will lose styles.
+  // Make sure to convert them to use `styled()` or `<Box />` first.
+  disableGlobal: true,
+  // Class names will receive this seed to avoid name collisions.
+  seed: 'mui-jss',
+});
+
+import React,  {useState, useEffect} from 'react';
+
+const columns = [
+  {
+    field : "id", headername : "id",
+    field : "name", headername : "name",
+    field : "username", headername : "username",
+    field : "email", headername : "email",
+    field : "phone", headername : "phone",
+    field : "website", headername : "website",
+
+  }
+];
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+useEffect (() => {
+   fetch('https://jsonplaceholder.typicode.com/users')
+     .then(resp => resp.json())
+     .then(data => {
+      setData(data)
+     })
+} ,[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataGrid
+      row= {data}
+      columns = {columns}
+      />
     </div>
   );
 }
